228df9a1d7c59ba4d0457357eb81bab5a7b20006
Stelviaa <forestiesacha@gmail.com>
2024-03-06 15:32:53 +0100

    mariadb okay to connect with rootPassword | soon wordpress

--- srcs/requirements/mariadb/conf/init.sh
+++ srcs/requirements/mariadb/conf/init.sh
@@ -2,19 +2,21 @@ if [ -d "/var/lib/mysql/$SQL_DATABASE" ]
 then
 	echo "database exists"
 else
-
-    mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --skip-test-db >> /dev/null
+    echo "creation of db"
+    
+    mysql_install_db --basedir=/usr --datadir=/var/lib/mysql/ --user=mysql --skip-test-db >> /dev/null
 
     mysqld -u mysql --bootstrap << EOF
         USE mysql;
         FLUSH PRIVILEGES;
         DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
-        ALTER USER 'root'@'localhost' IDENTIFIED BY $SQL_ROOT_PASSWORD;
+        ALTER USER 'root'@'localhost' IDENTIFIED BY '$SQL_ROOT_PASSWORD';
         CREATE DATABASE $SQL_DATABASE CHARACTER SET 'utf8' COLLATE 'utf8_general_ci';
-        CREATE USER $SQL_USER@'localhost' IDENTIFIED BY $SQL_PASSWORD;
-        GRANT ALL PRIVILEGES ON $SQL_DATABASE.* TO $SQL_USER@'%' IDENTIFIED BY $SQL_PASSWORD;
+        CREATE USER '$SQL_USER'@'localhost' IDENTIFIED BY '$SQL_PASSWORD';
+        GRANT ALL PRIVILEGES ON $SQL_DATABASE.* TO '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD';
         FLUSH PRIVILEGES;
 EOF
 
 fi
+echo "launch of db"
 exec mysqld --user=mysql --console
